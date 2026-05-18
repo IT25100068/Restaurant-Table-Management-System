090217381740b2db4ab5bf9a7ebd19c1bbc52c80
IT25104063 <it25104063@my.sliit.lk>
2026-05-18 13:22:41 +0530

    Create customer model classes with oop concepts(Encapsulations,inheritance,polymorphism).java

--- src/main/java/com/restaurant/restauranttablemanagementsystem/model/Customer.java
+++ src/main/java/com/restaurant/restauranttablemanagementsystem/model/Customer.java
@@ -22,44 +22,60 @@ public abstract class Customer {
     }
 
     public int getId() {
-        return id; }
+        return id; 
+    }
     public void setId(int id) {
-        this.id = id; }
+        this.id = id; 
+    }
 
     public String getName() {
-        return name; }
+        return name; 
+    }
     public void setName(String name) {
-        this.name = name; }
+        this.name = name; 
+    }
 
     public String getEmail() { 
-        return email; }
+        return email;
+    }
     public void setEmail(String email) {
-        this.email = email; }
+        this.email = email; 
+    }
 
     public String getPassword() {
-        return password; }
+        return password; 
+    }
     public void setPassword(String password) {
-        this.password = password; }
+        this.password = password;
+    }
 
     public String getPhone() {
-        return phone; }
+        return phone; 
+    }
     public void setPhone(String phone) {
-        this.phone = phone; }
+        this.phone = phone; 
+    }
 
     public String getAddress() {
-        return address; }
+        return address; 
+    }
     public void setAddress(String address) {
-        this.address = address; }
+        this.address = address; 
+    }
 
     public String getCustomerType() {
-        return customerType; }
+        return customerType; 
+    }
     public void setCustomerType(String customerType) {
-        this.customerType = customerType; }
+        this.customerType = customerType; 
+    }
 
     public int getLoyaltyPoints() {
-        return loyaltyPoints; }
+        return loyaltyPoints;
+    }
     public void setLoyaltyPoints(int loyaltyPoints) {
-        this.loyaltyPoints = loyaltyPoints; }
+        this.loyaltyPoints = loyaltyPoints; 
+    }
 
     public abstract double calculateDiscount(double amount);
     public abstract int addLoyaltyPoints(double amount);
