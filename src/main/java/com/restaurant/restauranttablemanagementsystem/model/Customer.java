ece560546536fb7fe45fb8dc9392e0648a787697
IT25104063 <it25104063@my.sliit.lk>
2026-05-18 13:11:31 +0530

    Update Customer.java

--- src/main/java/com/restaurant/restauranttablemanagementsystem/model/Customer.java
+++ src/main/java/com/restaurant/restauranttablemanagementsystem/model/Customer.java
@@ -21,29 +21,45 @@ public abstract class Customer {
         this.loyaltyPoints = 0;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id; }
+    public void setId(int id) {
+        this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name; }
+    public void setName(String name) {
+        this.name = name; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email; }
+    public void setEmail(String email) {
+        this.email = email; }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+        return password; }
+    public void setPassword(String password) {
+        this.password = password; }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getPhone() {
+        return phone; }
+    public void setPhone(String phone) {
+        this.phone = phone; }
 
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
+    public String getAddress() {
+        return address; }
+    public void setAddress(String address) {
+        this.address = address; }
 
-    public String getCustomerType() { return customerType; }
-    public void setCustomerType(String customerType) { this.customerType = customerType; }
+    public String getCustomerType() {
+        return customerType; }
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType; }
 
-    public int getLoyaltyPoints() { return loyaltyPoints; }
-    public void setLoyaltyPoints(int loyaltyPoints) { this.loyaltyPoints = loyaltyPoints; }
+    public int getLoyaltyPoints() {
+        return loyaltyPoints; }
+    public void setLoyaltyPoints(int loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints; }
 
     public abstract double calculateDiscount(double amount);
     public abstract int addLoyaltyPoints(double amount);
